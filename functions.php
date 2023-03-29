016b754fbe27877e648fac40290f36b93b2dd658
potatodevs <hoshiku1997@gmail.com>
2023-03-29 15:16:53 +0700

    function random_num-length

--- functions.php
+++ functions.php
@@ -17,7 +17,25 @@ function check_login($con) {
   }
 
   //redirect to login
-
   header("Location: login.php");
   die;
+}
+
+function random_num($length)
+{
+    $text = "";
+    if($length < 5)
+    {
+        $length = 5;
+    }
+
+    $len = rand(4,$length);
+
+    for ($i=0; $i < $len; $i++) { 
+        # code...
+
+        $text .= rand(0,9);
+    }
+
+    return $text;
 }
